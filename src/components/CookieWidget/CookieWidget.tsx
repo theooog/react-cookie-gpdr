030bf819fbcd6c66358dbe815f989608648ffbb3
theooog <theo-g@live.nl>
2023-05-05 13:14:21 +0200

    Second commit

--- src/components/CookieWidget/CookieWidget.tsx
+++ src/components/CookieWidget/CookieWidget.tsx
@@ -1,111 +1,186 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Cookies from "js-cookie";
 import "./CookieWidget.css";
 
 export interface CookieWidgetProps {
-    location?: "left" | "right";
-    color: string;
-    policyLink: string;
-    title?: string;
-    subtitle?: string;
-    text?: string;
-    policyLinkText?: string;
-    rejectButtonText?: string;
-    acceptButtonText?: string;
-    cookieSecurity?: boolean;
-    hideOnScrollDown?: boolean;
-    onAccept: () => void;
-    onReject: () => void;
+  location?: "left" | "right";
+  color: string;
+  policyLink: string;
+  title?: string;
+  subtitle?: string;
+  text?: string;
+  editLink?: string;
+  policyLinkText?: string;
+  rejectButtonText?: string;
+  acceptButtonText?: string;
+  cookieSecurity?: boolean;
+  hideOnScrollDown?: boolean;
+  onAccept: () => void;
+  onReject: () => void;
 }
 
+const [showAnalytics, setShowAnalytics] = React.useState(true);
 const cookieConsentName = "cookie_gpdr_consent";
 
 export const getCookieConsentValue = () => {
-    const cookieValue = Cookies.get(cookieConsentName);
+  const cookieValue = Cookies.get(cookieConsentName);
 
-    if (cookieValue === undefined) {
-        return Cookies.get(cookieConsentName);
-    }
+  if (cookieValue === undefined) {
+    return Cookies.get(cookieConsentName);
+  }
 
-    return cookieValue;
+  return cookieValue;
 };
 
 export const resetCookieConsentValue = (name = cookieConsentName) => {
-    Cookies.remove(name);
+  Cookies.remove(name);
 };
 
 const CookieWidget = (props: CookieWidgetProps) => {
+  const [isVisible, setIsVisible] = React.useState(false);
 
-    const [isVisible, setIsVisible] = React.useState(false);
+  const _handleScroll = (e: Event): void => {
+    console.log(window.scrollY);
+    if (isVisible && window.scrollY > 150) {
+      _onAccept();
+    }
+  };
 
-    const _handleScroll = (e: Event): void => {
+  useEffect(() => {
+    if (getCookieValue() === undefined) {
+      setIsVisible(true);
+
+      if (props.hideOnScrollDown) {
         console.log(window.scrollY);
-        if(isVisible && window.scrollY > 150) {
-            _onAccept();
-        }
+        window.addEventListener("scroll", _handleScroll, { passive: true });
+      }
+    }
+
+    return () => {
+      window.removeEventListener("scroll", _handleScroll);
     };
+  }, []);
 
-    useEffect(() => {
-        if (getCookieValue() === undefined) {
-            setIsVisible(true);
+  const setCookie = (cookieValue: string) => {
+    let { cookieSecurity } = props;
 
-            if (props.hideOnScrollDown) {
-                console.log(window.scrollY);
-                window.addEventListener("scroll", _handleScroll, { passive: true });
-            }
-        }
-
-        return () => {
-            window.removeEventListener("scroll", _handleScroll);
-        }
-    }, []);
-
-    const setCookie = (cookieValue: string) => {
-        let { cookieSecurity } = props;
-
-        if (cookieSecurity === undefined) {
-            cookieSecurity = location ? location.protocol === "https:" : true;
-        }
-
-        Cookies.set(cookieConsentName, cookieValue, { expires: 365, sameSite: 'lax', secure: cookieSecurity });
+    if (cookieSecurity === undefined) {
+      cookieSecurity = location ? location.protocol === "https:" : true;
     }
 
-    const getCookieValue = () => {
-        return getCookieConsentValue();
-    }
+    Cookies.set(cookieConsentName, cookieValue, {
+      expires: 365,
+      sameSite: "lax",
+      secure: cookieSecurity,
+    });
+  };
 
-    const _onAccept = () => {
-        setCookie("accepted");
-        props.onAccept();
-        setIsVisible(false);
-    }
+  const getCookieValue = () => {
+    return getCookieConsentValue();
+  };
 
-    const _onDecline = () => {
-        setCookie("declined");
-        props.onReject();
-        setIsVisible(false);
-    }
+  const _onAccept = () => {
+    setCookie("accepted");
+    props.onAccept();
+    setIsVisible(false);
+  };
 
-    if(!isVisible) return null;
+  const _onDecline = () => {
+    setCookie("declined");
+    props.onReject();
+    setIsVisible(false);
+  };
 
-    return (
-        <div className={"cookie_widget_container " + (props.location === 'right' ? "is-right" : "is-left")}>
-            <div className="cookie_widget_shape_1" style={{background: props.color}}></div>
-            <div className="cookie_widget_shape_2"></div>
-            <div className="cookie_widget_heading">
-                <p className="cookie_widget_title">{props.title ?? 'This website'}</p>
-                <p className="cookie_widget_subtitle">{props.subtitle ?? 'use Cookies'}</p>
-            </div>
-            <div className="cookie_widget_content">
-                <p className="cookie_widget_text">{props.text ?? 'We use cookies and similar methods to recognize visitors and remember their preferences. We also use them to measure ad campaign effectiveness, target ads and analyze site traffic. To learn more about these methods, including how to disable them, view our Cookie Policy.'}</p>
-                {props.policyLink && <a className="cookie_widget_privacy_policy" href={props.policyLink} target="_blank" style={{color: props.color}}>{props.policyLinkText ?? 'Read the privacy policy'}</a>}
-            </div>
-            <div className="cookie_widget_footer">
-                <button className="cookie_widget_button_reject" aria-label={props.rejectButtonText ?? "Reject All"} onClick={_onDecline}>{props.rejectButtonText ?? "Reject All"}</button>
-                <button className="cookie_widget_button_accept" aria-label={props.acceptButtonText ?? "Accept All"} style={{color: props.color}} onClick={_onAccept}>{props.acceptButtonText ?? "Accept All"}</button>
-            </div>
-        </div>
-    );
+  if (!isVisible) return null;
+
+  const _onEditPreferences = () => {
+    setShowAnalytics(false);
+  };
+  const cookieText = showAnalytics
+    ? props.text ??
+      "We use cookies and similar methods to recognize visitors and remember their preferences. We also use them to measure ad campaign effectiveness, target ads and analyze site traffic. To learn more about these methods, including how to disable them, view our Cookie Policy."
+    : "Analytical cookies and technologies help us understand how people interact with our website. This helps us improve our website, apps, and communications, and helps us show you interesting and relevant content.";
+
+  const buttonText = showAnalytics ? "Reject All" : "Apply Changes";
+
+  return (
+    <div
+      className={
+        "cookie_widget_container " +
+        (props.location === "right" ? "is-right" : "is-left")
+      }
+    >
+      <div
+        className="cookie_widget_shape_1"
+        style={{ background: props.color }}
+      ></div>
+      <div className="cookie_widget_shape_2"></div>
+      <div className="cookie_widget_heading">
+        <p className="cookie_widget_title">{props.title ?? "This website"}</p>
+        <p className="cookie_widget_subtitle">
+          {props.subtitle ?? "use Cookies"}
+        </p>
+      </div>
+      <div className="cookie_widget_content">
+        <p className="cookie_widget_text">
+          {cookieText}
+          {props.editLink && (
+            <>
+              <br />
+              <a
+                href={props.editLink}
+                onClick={_onEditPreferences}
+                style={{ color: props.color, textDecoration: "underline" }}
+              >
+                Edit Preferences
+              </a>
+            </>
+          )}
+        </p>
+        {showAnalytics && (
+          <div className="cookie_widget_switch_container">
+            <span>Analytics:</span>
+
+            <label class="toggle">
+              <input
+                type="checkbox"
+                checked={showAnalytics}
+                onChange={() => setShowAnalytics(!showAnalytics)}
+              />
+              <span class="slider"></span>
+            </label>
+          </div>
+        )}
+        {props.policyLink && (
+          <a
+            className="cookie_widget_privacy_policy"
+            href={props.policyLink}
+            target="_blank"
+            style={{ color: props.color }}
+          >
+            {(props.policyLinkText && !isVisible) ?? "Read the privacy policy"}
+          </a>
+        )}
+      </div>
+      <div className="cookie_widget_footer">
+        <button
+          className="cookie_widget_button_reject"
+          aria-label={props.rejectButtonText ?? "Reject All"}
+          onClick={_onDecline}
+        >
+          {props.rejectButtonText ?? "Reject All"}
+        </button>
+        <button
+          className="cookie_widget_button_accept"
+          aria-label={props.acceptButtonText ?? "Accept All"}
+          style={{ color: props.color }}
+          onClick={_onAccept}
+        >
+          {props.acceptButtonText ?? "Accept All"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CookieWidget;
